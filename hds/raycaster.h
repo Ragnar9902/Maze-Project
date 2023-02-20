341cd565ed8f577422bcb360a8f0a16b7f86561d
Vozudo <jploperatobon@gmail.com>
2023-02-19 23:13:31 -0500

    Map correction & delete extra files

--- hds/raycaster.h
+++ hds/raycaster.h
@@ -17,8 +17,8 @@
 extern int worldMap[mapWidth][mapHeight];
 
 // Initial values
-#define WIN_X           2000
-#define WIN_Y           1200
+#define WIN_X           1280
+#define WIN_Y           720
 #define INIT_P_POS_X    23
 #define INIT_P_POS_Y    10
 #define INIT_P_DIR_X    -1
